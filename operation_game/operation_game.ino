25d865d68b399a61349d8e2533f1c6c3fc5c69b4
Salvatore Carotenuto <ultimoistante@gmail.com>
2021-03-02 12:33:41 +0100

    added schematic; resized images;

--- operation_game/operation_game.ino
+++ operation_game/operation_game.ino
@@ -10,6 +10,33 @@
  ** on December 16, 2018
  **/
 
+ // -----------------------------------------------------------------------------
+ // -----------------------------------------------------------------------------
+ //
+ // Arduino Nano connections
+ //                                    +-----+
+ //                              +-----|=====|-----+
+ //       Red LED ---> D13 (PB5) |o    |=====|    o| (PB4) D12
+ //                          3v3 |o    +-----+    o| (PB3) D11 ---> Speaker
+ //                         Vref |o               o| (PB2) D10 
+ //                     A0 (PC0) |o               o| (PB1) D9
+ //                     A1 (PC1) |o               o| (PB0) D8  
+ //                     A2 (PC2) |o               o| (PD7) D7
+ //                     A3 (PC3) |o    Arduino    o| (PD6) D6  
+ //                     A4 (PC4) |o      Nano     o| (PD5) D5
+ //                     A5 (PC5) |o               o| (PD4) D4
+ //                    A6 (ADC6) |o               o| (PD3) D3
+ //                    A7 (ADC7) |o               o| (PD2) D2  ---> Tweezers
+ //                           5V |o               o| GND
+ //                    RST (PC6) |o               o| (PC6) RST
+ //                          GND |o     . . .     o| (PD0) RX
+ //                          Vin |o     . . .     o| (PD1) TX
+ //                              +-----------------+
+ //
+ // -----------------------------------------------------------------------------
+ // -----------------------------------------------------------------------------
+
+
 #include <PCM.h>
 #include "sample_data.h"
 
@@ -38,13 +65,13 @@ unsigned int led_off_time_counter = LED_BLINK_OFF_TIME_MSEC;
 // Interrupt handler called once a millisecond
 SIGNAL(TIMER0_COMPA_vect)
 	{
-	if(blink_led) // if we have to blink...
+	if (blink_led) // if we have to blink...
 		{
-		if(!led_status)	// if LED is currently OFF
+		if (!led_status)	// if LED is currently OFF
 			{
 			// decreases milliseconds counter
 			led_off_time_counter--;
-			if(led_off_time_counter == 0) // if has passed defined time
+			if (led_off_time_counter == 0) // if has passed defined time
 				{
 				// activates LED, sets status to 1 and resets counter
 				digitalWrite(ledPin, HIGH);
@@ -56,7 +83,7 @@ SIGNAL(TIMER0_COMPA_vect)
 			{
 			// decreases milliseconds counter
 			led_on_time_counter--;
-			if(led_on_time_counter == 0) // if has passed defined time
+			if (led_on_time_counter == 0) // if has passed defined time
 				{
 				// deactivates LED, sets status to 0 and resets counter
 				digitalWrite(ledPin, LOW);
@@ -92,10 +119,10 @@ void setup()
 void loop()
 	{
 	// checks if it's passed enough time after last sample playback
-	if(millis() > (playbackStartTime + sampleDurationMillis))
+	if (millis() > (playbackStartTime + sampleDurationMillis))
 		{
 		// if status of sensePin goes low...
-		if(!digitalRead(sensePin))
+		if (!digitalRead(sensePin))
 			{
 			// ...activates LED blinking
 			blink_led = 1;
@@ -112,7 +139,7 @@ void loop()
 		}
 	//
 	// checks if it's time to stop LED blinking (sample playback end)
-	if(blink_led && millis() > playbackEndTime)
+	if (blink_led && millis() > playbackEndTime)
 		{
 		blink_led = 0;
 		digitalWrite(ledPin, LOW);
